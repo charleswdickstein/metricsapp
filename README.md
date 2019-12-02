9f78d02c00436a0f7b31437eff0527708173f0d1
charleswdickstein <charleswdickstein@gmail.com>
2019-12-01 23:20:30 -0500

    Reads Data from a generic file called data.txt containing JSON and saves it to the database

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # metrify
+## Install Django 
+pip install Django 
+
+## Scraper 
+Reads Data from a generic file called data.txt
+containing JSON and saves it to the database
+
+JSON File should be in this format:
+{"counter": 
+	{
+		"key1":"2",
+		"key2":"2",
+		"key3":"3",
+		...
+	}
+}
+
+## Demo Instructions
+### Run the following commands from the mysite directory in terminal to build DB:
+$ python manage.py migrate
+$ python manage.py makemigrations polls
+
+### Run command to start server. Then go to localhost:8000
+$ python manage.py runserver
+
+
+## TODO:
+### Counter application to create Dictionary and exposed JSON file which the scraper will read. 
+### Read Data from SQLite DB for data visualization
--- mysite/visualizer/views.py
+++ mysite/visualizer/views.py
@@ -1,4 +1,25 @@
 from django.http import HttpResponse
+import json
+# import json
+from .models import Counter
+import os
+from django.utils import timezone
+
+def process_data(file_path):
+	with open(file_path, 'r') as f:
+		datastore = json.load(f)
+		counter = datastore["counter"]
+		for key, value in counter.iteritems():
+			print(key, value)
+			counter = Counter(key=str(key), value=int(value), pub_date=timezone.now())
+			counter.save()
+	print(Counter.objects.all())
+	return datastore
 
 def home(request):
-    return HttpResponse('Hello, World!')
+	x = 1
+	
+	module_dir = os.path.dirname(__file__)  # get current directory
+	file_path = os.path.join(module_dir, 'data.txt')
+	datastore = process_data(file_path)
+	return HttpResponse(datastore["counter"])
