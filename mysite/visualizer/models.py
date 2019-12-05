2bb082a825167617b8bf7eb1aeb25ee2cb321ddf
charleswdickstein <charleswdickstein@gmail.com>
2019-12-05 16:39:47 -0500

    converted sqlite to pandas dataframe

--- mysite/visualizer/models.py
+++ mysite/visualizer/models.py
@@ -4,7 +4,19 @@ class Counter(models.Model):
     key = models.CharField(max_length=200)
     value = models.IntegerField(default=0)
     pub_date = models.DateTimeField('date published')
+    #url = models.CharField(max_length=200, default="none")
     def __str__(self):
         return self.key
+	
+	class Meta:
+		db_table = 'COUNTERS'
+	
+	def to_dict(self):
+		return {
+			'key': self.key,
+			'y': [self.value, self.pub_date]
+		}
+
+
 
   
--- mysite/visualizer/views.py
+++ mysite/visualizer/views.py
@@ -4,16 +4,20 @@ import json
 from .models import Counter
 import os
 from django.utils import timezone
+import pandas as pd
+import sqlite3
+
 
 def process_data(file_path):
+	t = timezone.now()
 	with open(file_path, 'r') as f:
 		datastore = json.load(f)
 		counter = datastore["counter"]
 		for key, value in counter.iteritems():
 			print(key, value)
-			counter = Counter(key=str(key), value=int(value), pub_date=timezone.now())
+			counter = Counter(key=str(key), value=int(value), pub_date=t)
 			counter.save()
-	print(Counter.objects.all())
+	
 	return datastore
 
 def home(request):
@@ -22,4 +26,10 @@ def home(request):
 	module_dir = os.path.dirname(__file__)  # get current directory
 	file_path = os.path.join(module_dir, 'data.txt')
 	datastore = process_data(file_path)
+	print("Data")
+	
+	df = pd.DataFrame(list(Counter.objects.all().values()))
+	df = pd.DataFrame(list(Counter.objects.all().values('key', 'value', 'pub_date')))
+	print(df.head())
+	
 	return HttpResponse("Write Successful")
